dd8e69afabc10a2e6fb6616fd8cbbcfd73b21ade
Hemant Pawar <hpawar@dwavesys.com>
2020-05-13 18:30:38 +0000

    Updated copyright notice , removed change dir before execution

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,16 +1,16 @@
-#    Copyright 2020 D-Wave Systems Inc.
-
-#    Licensed under the Apache License, Version 2.0 (the "License")
-#    you may not use this file except in compliance with the License.
-#    You may obtain a copy of the License at
-
-#        http: // www.apache.org/licenses/LICENSE-2.0
-
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS,
-#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#    See the License for the specific language governing permissions and
-#    limitations under the License.
+# Copyright 2020 D-Wave Systems Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 from subprocess import Popen, PIPE,STDOUT
 import unittest
@@ -19,8 +19,7 @@ import os
 class IntegrationTests(unittest.TestCase):
 
     def test_factoring(self):
-        cwd = os.getcwd()
-        p = Popen(["python", cwd+"/demo.py"], stdout=PIPE, stdin=PIPE, stderr=STDOUT)    
+        p = Popen(["python","demo.py"], stdout=PIPE, stdin=PIPE, stderr=STDOUT)    
         p.stdin.write(b'49\n')
         output = p.communicate()[0]
         output = str(output).upper()
