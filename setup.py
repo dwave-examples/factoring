7f169f171c1b8f61f09c1c2b2a1dd431df018eeb
Joel Pasvolsky <jpasvolsky@dwavesys.com>
2020-10-23 14:25:26 -0700

    Update setup install_requires

--- setup.py
+++ setup.py
@@ -14,12 +14,8 @@ exec(open(path_to_package_info).read())
 
 # These should be minimal requiments for the package to work, and avoid pinning dependencies unless required. See
 # https://packaging.python.org/discussions/install-requires-vs-requirements/
-install_requires = ['dwavebinarycsp>=0.0.2,<0.1.0',
-                    'dwave-system>=0.6.0,<0.7.0',
-                    'minorminer>=0.1.7,<0.2.0',
-                    'dimod>=0.7.2,<0.8.0',
-                    'dwave-cloud-client>=0.5.1',
-                    'jsonschema>=2.6.0,<3.0.0']
+install_requires = ['dwave-ocean-sdk>=3.0.0',
+                    'jsonschema==3.2.0']
 
 # Any extra requirements, to be used by pip install PACKAGENAME['keyname']
 extras_require = {}
